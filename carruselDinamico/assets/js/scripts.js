0476d3e9460db97488c85acfbdc02b7ce8a62e22
Francisco Diaz <fjdiaz86@gmail.com>
2020-04-23 12:38:19 -0500

    Carrusel dinamico

---
+++ carruselDinamico/assets/js/scripts.js
@@ -0,0 +1,22 @@
+$('#carouselExampleControls .carousel').carousel()
+var dominio = window.location.protocol + "//" + window.location.host;
+$(".clientes").on('click', function(evento){
+    evento.preventDefault();
+    const cliente = $(this).data('cliente');
+    console.log(cliente);
+    $.ajax({
+        data: {cliente:cliente},
+        type: "POST",
+        url:  dominio + "/carruselDinamico/php/clientes.php",
+        cache: false,
+        success: function (retorno) {
+            const modal = $("#modal1 .modal-content .modal-body");
+            modal.empty();
+            modal.html(retorno);
+            $("#modal1").modal('show');
+        },
+        error: function(retorno){
+
+        }
+    });
+});
